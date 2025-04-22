072403d062b30b5a9964c48877fd4b75faebce79
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-04-21 23:33:26 -0300

    Feat: Se refactorizó el componente SheetMenu para aceptar propiedades dinámicas y mejorar la flexibilidad

--- src/components/sheet-menu.tsx
+++ src/components/sheet-menu.tsx
@@ -9,7 +9,14 @@ import {
 } from "@/components/ui/sheet";
 import { useState } from "react";
 
-export function SheetMenu() {
+interface SheetMenuProps {
+    routes: { name: string; href: string }[];
+    title: string;
+    description?: string;
+}
+
+export function SheetMenu(props: SheetMenuProps) {
+    const { routes, title, description } = props;
     const [open, setOpen] = useState(false);
     return (
         <Sheet open={open} onOpenChange={setOpen}>
@@ -20,7 +27,7 @@ export function SheetMenu() {
                         className="size-5"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke="currentColor"
+                        stroke="#000000"
                         strokeWidth="2"
                     >
                         <path
@@ -31,81 +38,24 @@ export function SheetMenu() {
                     </svg>
                 </Button>
             </SheetTrigger>
-            <SheetContent>
+            <SheetContent className="bg-white/80 border-white/50 backdrop-blur-sm shadow-lg dark:bg-zinc-900/50 dark:backdrop-blur-sm dark:shadow-lg">
                 <SheetHeader>
-                    <SheetTitle>El Rinconcito</SheetTitle>
-                    <SheetDescription></SheetDescription>
+                    <SheetTitle>{title}</SheetTitle>
+                    <SheetDescription>{description}</SheetDescription>
                 </SheetHeader>
                 <nav aria-label="Global">
-                    <ul className="flex flex-col items-start font-bold gap-6 text-xl">
-                        <li>
-                            <a
-                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
-                                href="/#hero"
-                                onClick={() => setOpen(false)}
-                            >
-                                Home
-                            </a>
-                        </li>
-
-                        <li>
-                            <a
-                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
-                                href="/#Descriptions"
-                                onClick={() => setOpen(false)}
-                            >
-                                Descripción
-                            </a>
-                        </li>
-
-                        <li>
-                            <a
-                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
-                                href="/#WhyUs"
-                                onClick={() => setOpen(false)}
-                            >
-                                Porque elegirnos
-                            </a>
-                        </li>
-
-                        <li>
-                            <a
-                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
-                                href="/#Service"
-                                onClick={() => setOpen(false)}
-                            >
-                                Servicios
-                            </a>
-                        </li>
-
-                        <li>
-                            <a
-                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
-                                href="/#Testimonials"
-                                onClick={() => setOpen(false)}
-                            >
-                                Testimonios
-                            </a>
-                        </li>
-
-                        <li>
-                            <a
-                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
-                                href="/#Reserva"
-                                onClick={() => setOpen(false)}
-                            >
-                                Contáctenos
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
-                                href="/#Faqs"
-                                onClick={() => setOpen(false)}
-                            >
-                                Pregunta frecuentes
-                            </a>
-                        </li>
+                    <ul className="flex flex-col items-start font-bold gap-6 text-xl ml-4">
+                        {routes.map((route) => (
+                            <li key={route.name}>
+                                <a
+                                    className="text-zinc-900 transition hover:text-zinc-900/75 font-semibold"
+                                    href={route.href}
+                                    onClick={() => setOpen(false)}
+                                >
+                                    {route.name}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
             </SheetContent>
